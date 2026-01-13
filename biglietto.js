df35567159930024287364cedd5976ee3b9b981c
AntonioCappellano <antoniocappellano97@gmail.com>
2026-01-13 14:38:26 +0100

    modify html and add const of form

--- biglietto.js
+++ biglietto.js
@@ -1,6 +1,11 @@
+const isTicketForm = document.getElementById("ticket-form");
 const isNumberKm = document.getElementById("number-km");
 const isUserAge = document.getElementById("user-age");
 const sendBtn = document.getElementById("send-btn");
 console.log(isNumberKm);
 console.log(isUserAge);
 console.log(sendBtn);
+
+isTicketForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+});
