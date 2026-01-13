c5a0e91d98651c18525af4c0f2cf9f9c554c689c
AntonioCappellano <antoniocappellano97@gmail.com>
2026-01-13 15:22:06 +0100

    add const in Event Listener

--- biglietto.js
+++ biglietto.js
@@ -1,11 +1,18 @@
 const isTicketForm = document.getElementById("ticket-form");
-const isNumberKm = document.getElementById("number-km");
-const isUserAge = document.getElementById("user-age");
+const isNumberKm = document.getElementById("kilometre-input");
+const isUserAge = document.getElementById("age-input");
 const sendBtn = document.getElementById("send-btn");
+
+console.log(isTicketForm);
 console.log(isNumberKm);
 console.log(isUserAge);
 console.log(sendBtn);
 
 isTicketForm.addEventListener("submit", (event) => {
   event.preventDefault();
+  console.log(event);
+  const eta = isUserAge.value;
+  console.log(eta);
+  const kilometres = isNumberKm.value;
+  console.log(kilometres);
 });
