3a2428a8011003f3a779f6067de8e021de5a1691
AntonioCappellano <antoniocappellano97@gmail.com>
2026-01-13 14:08:52 +0100

    insert const in js

---
+++ biglietto.js
@@ -0,0 +1,6 @@
+const isNumberKm = document.getElementById("number-km");
+const isUserAge = document.getElementById("user-age");
+const sendBtn = document.getElementById("send-btn");
+console.log(isNumberKm);
+console.log(isUserAge);
+console.log(sendBtn);
