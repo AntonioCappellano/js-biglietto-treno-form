5977982d4fc4c59757197905c41c84182e3d8f4a
AntonioCappellano <antoniocappellano97@gmail.com>
2026-01-13 15:32:20 +0100

    add logic total price in a Event Listener

--- biglietto.js
+++ biglietto.js
@@ -15,4 +15,16 @@ isTicketForm.addEventListener("submit", (event) => {
   console.log(eta);
   const kilometres = isNumberKm.value;
   console.log(kilometres);
+
+  const prezzoPerKm = 0.21;
+  let totalPrice = kilometres * prezzoPerKm;
+  if (eta < 18) {
+    totalPrice = totalPrice - (totalPrice * 20) / 100;
+  } else if (eta > 65) {
+    totalPrice = totalPrice - (totalPrice * 40) / 100;
+  }
+
+  totalPrice = totalPrice.toFixed(2);
+
+  console.log(`Prezzo finale: €${totalPrice}`);
 });
